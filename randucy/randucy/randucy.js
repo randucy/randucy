1896bf4b4a29aab8d2c0857fbb22a92e1fb16180
Thijs Boom <twboom@hotmail.nl>
2021-02-03 10:35:37 +0100

    Possible bugfix

--- randucy/randucy/randucy.js
+++ randucy/randucy/randucy.js
@@ -1,4 +1,4 @@
-help = function() {
+randucy.help = function() {
   randucy.newLine('||========[ RanDucy Help ]========||')
   for (let i = 0; i < randucy.data.help.length; i++) {
     randucy.newLine(randucy.data.help[i].name + ': ' + randucy.data.help[i].description + ' ' +  '[' + randucy.data.help[i].usage + ']')
